2eee358b84c77cd2d548e3a37773fc0e9665a872
benjamin-summers <summerstyler2@gmail.com>
2018-09-12 18:33:15 -0400

    added files to github

---
+++ assets/js/autorun.js
@@ -0,0 +1,7 @@
+function autorun()
+{
+
+}
+if (document.addEventListener) document.addEventListener("DOMContentLoaded", autorun, false);
+else if (document.attachEvent) document.attachEvent("onreadystatechange", autorun);
+else window.onload = autorun;
